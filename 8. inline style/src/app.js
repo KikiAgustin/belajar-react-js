ba2bf60a381b697b094c4a655b4e28cd9206392e
kikiAgustin <kikiagustin62@gmail.com>
2022-08-19 20:21:30 +0600

    add folder learning react js

---
+++ 8. inline style/src/app.js
@@ -0,0 +1,16 @@
+const root = document.querySelector('#root');
+
+const element = (
+    <div
+        style={{
+            width: '200px',
+            height: '200px',
+            backgroundColor: 'blue'
+        }}
+    >
+
+    </div>
+)
+
+ReactDOM.render(element, root);
+

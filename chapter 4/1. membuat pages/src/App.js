3ded1da7b38c678bf4cf1e6a087f953c62d5d929
kikiAgustin <kikiagustin62@gmail.com>
2022-08-31 07:30:46 +0600

    learning the real project

---
+++ chapter 4/1. membuat pages/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import Profile from './pages/Profile';
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/profile" >Profile</Link></li>
+        <li><Link to="/blog" >Blog</Link></li>
+        <li><Link to="/contact" >Contact</Link></li>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ chapter 4/2. detail page/src/pages/Contact.js
@@ -0,0 +1,16 @@
+export default function Contact() {
+    return (
+        <section>
+            <h1>kalian bisa menghubungi aku pada contact yang tertera dibawah</h1>
+            <ul>
+                <li>WA: 085667887665</li>
+                <li>Email: kikiagustin@pinterbaget.com</li>
+            </ul>
+            <h1>Ato kalian bisa melihat saya pansos di sosmed</h1>
+            <ul>
+                <li><a href="www.facebook.com" />Facebook</li>
+                <li><a href="ww.instagram.com/kikiagustiiin" />Instagram</li>
+            </ul>
+        </section>
+    );
+}
